36e7334b7c95531a556e2a83767b8c13f18fb156
safa-hathroubi <safa.hathroubi@esprit.tn>
2023-03-23 11:25:49 +0100

    Started adding bottom nav bar + fixed some bugs

--- Esprit_Space/HomeView.swift
+++ Esprit_Space/HomeView.swift
@@ -75,115 +75,163 @@ struct EventRow: View {
 
 
 import SwiftUI
+/*
+import BottomBar_SwiftUI
 
-struct Event: Identifiable {
-    let id = UUID()
-    let name: String
-    let image: String
-    let date: Date
-    let organizer: String
-    let description: String
-    let price: Double
-    //let PS: String
-}
 
-struct HomeView: View {
-    let events = [        Event(name: "Ramadhan Iftar", image: "ramadan-iftar", date: Date(), organizer: "Esprit Engineering", description: "Celebrate the start of Ramadhan with a delicious iftar shared together with your friends. PS: Hot mean + dessert included in the menu!", price: 10.00),        Event(name: "Charityyy Run", image: "Marathon_mock_poster", date: Date(), organizer: "Esprit IEEE Club", description: "Join us for a 5k run to raise money for local charities. All ages and skill levels welcome! ", price: 5.00)    ]
+// array of items for the bottom bar
+let items: [BottomBarItem] = [
+    BottomBarItem(icon: "house.fill", title: "Home", color: .purple),
+    BottomBarItem(icon: "heart", title: "Likes", color: .pink),
+    BottomBarItem(icon: "magnifyingglass", title: "Search", color: .orange),
+    BottomBarItem(icon: "person.fill", title: "Profile", color: .blue)
+]
+
+struct BasiccView: View {
+    let item: BottomBarItem
     
-    var body: some View {
-        NavigationView {
-            List(events) { event in
-                NavigationLink(destination: EventDetail(event: event)) {
-                    EventRow(event: event)
+    var detailText: String {
+        "\(item.title) Detail"
+    }
+    
+    */
+    
+    
+    
+    
+    
+    
+    struct Event: Identifiable {
+        let id = UUID()
+        let name: String
+        let image: String
+        let date: Date
+        let organizer: String
+        let description: String
+        let price: Double
+        //let PS: String
+    }
+    
+    struct HomeView: View {
+        let events = [        Event(name: "Ramadhan Iftar", image: "ramadan-iftar", date: Date(), organizer: "Esprit Engineering", description: "Celebrate the start of Ramadhan with a delicious iftar shared together with your friends. PS: Hot mean + dessert included in the menu!", price: 10.00),        Event(name: "Charityyy Run", image: "Marathon_mock_poster", date: Date(), organizer: "Esprit IEEE Club", description: "Join us for a 5k run to raise money for local charities. All ages and skill levels welcome! ", price: 5.00)    ]
+        
+        var body: some View {
+            NavigationView {
+                List(events) { event in
+                    NavigationLink(destination: EventDetail(event: event)) {
+                        EventRow(event: event)
+                    }
                 }
-            }
-            .navigationTitle("Events")
-        }
-    }
-}
-
-struct EventRow: View {
-    let event: Event
-    
-    var body: some View {
-        HStack {
-            Image(event.image)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 80, height: 80)
-                .cornerRadius(10)
-            
-            VStack(alignment: .leading) {
-                Text(event.name)
-                    .font(.headline)
-                Text(event.organizer)
-                    .font(.subheadline)
-                Text(event.date, style: .date)
-                    .font(.subheadline)
+                .navigationTitle("Home/Events")
             }
         }
-        .padding(.vertical, 8)
     }
-}
-
-struct EventDetail: View {
-    let event: Event
     
-    var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
+    struct EventRow: View {
+        let event: Event
+        
+        var body: some View {
+            HStack {
                 Image(event.image)
                     .resizable()
-                    .aspectRatio(contentMode: .fit)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80)
                     .cornerRadius(10)
-                    .padding(.horizontal)
                 
-                Text(event.name)
-                    .font(.title)
-                    .padding()
-                
-                HStack {
-                    Text("Organizer:")
+                VStack(alignment: .leading) {
+                    Text(event.name)
                         .font(.headline)
                     Text(event.organizer)
                         .font(.subheadline)
-                }
-                .padding(.horizontal)
-                
-                HStack {
-                    Text("Date:")
-                        .font(.headline)
                     Text(event.date, style: .date)
                         .font(.subheadline)
                 }
-                .padding(.horizontal)
-                
-                HStack {
-                    Text("Price:")
-                        .font(.headline)
-                    Text("$\(event.price, specifier: "%.2f")")
-                        .font(.subheadline)
+            }
+            .padding(.vertical, 8)
+        }
+    }
+    
+    struct EventDetail: View {
+        let event: Event
+        
+        var body: some View {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Image(event.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    
+                    Text(event.name)
+                        .font(.title)
+                        .padding()
+                    
+                    HStack {
+                        Text("Organizer:")
+                            .font(.headline)
+                        Text(event.organizer)
+                            .font(.subheadline)
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Text("Date:")
+                            .font(.headline)
+                        Text(event.date, style: .date)
+                            .font(.subheadline)
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Text("Price:")
+                            .font(.headline)
+                        Text("$\(event.price, specifier: "%.2f")")
+                            .font(.subheadline)
+                    }
+                    .padding(.horizontal)
+                    
+                    Text(event.description)
+                        .padding()
+                    
+                    Spacer()
                 }
-                .padding(.horizontal)
-                
-                Text(event.description)
-                    .padding()
-                
-                Spacer()
+            }
+            .navigationBarTitle(event.name)
+        }
+    }
+    
+    
+  
+
+/*
+    struct ContentView : View {
+        @State private var selectedIndex: Int = 0
+        
+        var selectedItem: BottomBarItem {
+            items[selectedIndex]
+        }
+        
+        var body: some View {
+            NavigationView {
+                VStack {
+                    BasiccView(item: selectedItem)
+                        .navigationBarTitle(Text(selectedItem.title))
+                    BottomBar(selectedIndex: $selectedIndex, items: items)
+                }
             }
         }
-        .navigationBarTitle(event.name)
+    }*/
+    
+    
+    
+    
+    
+    
+    
+    struct HomeView_Previews: PreviewProvider {
+        static var previews: some View {
+            HomeView()
+        }
     }
-}
-
-
-
-
-
-
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
-}
+//}
